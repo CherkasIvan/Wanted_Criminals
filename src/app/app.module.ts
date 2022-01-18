46c135d4a059fd2150d7238a8c697a38e38e3a58
Ivan Cherkas <distor6on@yandex.ru>
2022-01-18 12:20:49 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProfileSelectionComponent } from './components/header/profile-selection/profile-selection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProfileSelectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/header/profile-selection/profile-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fw-profile-selection',
+  templateUrl: './profile-selection.component.html',
+  styleUrls: ['./profile-selection.component.scss']
+})
+export class ProfileSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
