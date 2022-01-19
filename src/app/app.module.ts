fae50e3b8261690ecdc229e3a0cccfe46ae1a0c4
Ivan Cherkas <distor6on@yandex.ru>
2022-01-19 07:44:28 +0300

    init feture/login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppMaterialModule } from './modules/app-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,12 +7,23 @@ import { HeaderComponent } from './components/header/header.component';
 import { ProfileSelectionComponent } from './components/header/profile-selection/profile-selection.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './pages/login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ProfileSelectionComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ProfileSelectionComponent,
+    LoginComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'profile', component: HeaderComponent },
+    ]),
     BrowserAnimationsModule,
     AppMaterialModule,
   ],
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'fw-login',
@@ -11,7 +13,28 @@ export class LoginComponent implements OnInit {
   email = '';
   password = '';
 
-  constructor() {}
+  // constructor(private http: HttpClient) {}
+
+  // login(login: string, password: string): Observable<any> {
+  //   let headers = new HttpHeaders();
+  //   headers = headers.append(
+  //     'Content-Type',
+  //     'application/x-www-form-urlencoded'
+  //   );
+
+  //   let params = new HttpParams();
+  //   params = params.append('grant_type', 'password');
+  //   params = params.append('username', login);
+  //   params = params.append('password', password);
+  //   params = params.append('client_id', 'HEHApiClient');
+  //   params = params.append('scope', 'heh_api');
+
+  //   return this.http.post(`${users-auth}/connect/token`, params, { headers });
+  // }
+
+  // getToken(): any {
+  //   return localStorage.getItem('isAuth');
+  // }
 
   ngOnInit(): void {}
 }
